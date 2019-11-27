440c898eb39cea917b1d1289931e343f3eedcdaa
vishal babu <vishal.babu@blackstraw.ai>
2019-11-27 16:04:56 +0530

    code cleansing done

--- eng_german.py
+++
@@ -1,4 +0,0 @@
-import torch
-#torch.hub.list('pytorch/fairseq')
-en2de = torch.hub.load('pytorch/fairseq', 'transformer.wmt16.en-de', tokenizer='moses', bpe='subword_nmt')
-print(en2de.translate('Tejal is my best buddy'))
--- vision/ssd_detection.py
+++ vision/ssd_detection.py
@@ -33,32 +33,6 @@ def get_detection(ssd_model, utils , img_list):
     best_results_per_input = [utils.pick_best(results, 0.40) for results in results_per_input]
     return best_results_per_input
     
-    
-    
-def get_aaj(ssd_model, utils , uris):
-    ssd_model.to('cuda')
-    ssd_model.eval()
-    inputs = [utils.prepare_input(uri) for uri in uris]
-    tensor = utils.prepare_tensor(inputs, precision == 'fp16')
-    detections_batch = ssd_model(tensor)
-    results_per_input = utils.decode_results(detections_batch)
-    best_results_per_input = [utils.pick_best(results, 0.40) for results in results_per_input]
-    classes_to_labels = utils.get_coco_object_dictionary()
-    for image_idx in range(len(best_results_per_input)):
-        fig, ax = plt.subplots(1)
-        # Show original, denormalized image...
-        image = inputs[image_idx] / 2 + 0.5
-        ax.imshow(image)
-        # ...with detections
-        bboxes, classes, confidences = best_results_per_input[image_idx]
-        for idx in range(len(bboxes)):
-            left, bot, right, top = bboxes[idx]
-            x, y, w, h = [val * 300 for val in [left, bot, right - left, top - bot]]
-            rect = patches.Rectangle((x, y), w, h, linewidth=1, edgecolor='r', facecolor='none')
-            ax.add_patch(rect)
-            ax.text(x, y, "{} {:.0f}%".format(classes_to_labels[classes[idx] - 1], confidences[idx]*100), bbox=dict(facecolor='white', alpha=0.5))
-    plt.show()
-
 if __name__ == "__main__":
     precision = 'fp32'
     img_list = ["/media/bharatforge/Ubuntu_data/vishal/datasets/VOC2012/JPEGImages/2007_000027.jpg",
@@ -66,6 +40,6 @@ if __name__ == "__main__":
     utils = torch.hub.load('NVIDIA/DeepLearningExamples:torchhub', 'nvidia_ssd_processing_utils')
     ssd_model = torch.hub.load('NVIDIA/DeepLearningExamples:torchhub', 'nvidia_ssd', model_math=precision)
     best_results_per_input = get_detection(ssd_model, utils, img_list)
-    print(best_results_per_input)
+    #print(best_results_per_input)
     classes_to_labels = utils.get_coco_object_dictionary()
     print_detection(best_results_per_input, classes_to_labels, img_list)
