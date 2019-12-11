24a0694eb3a04b9bfc558b0a27ee9f07a1a07d52
VishalChak <vishalbabu.in@gmail.com>
2019-12-11 11:22:33 +0530

    added to run on gpu code

--- tutorials/img_classifier.py
+++ tutorials/img_classifier.py
@@ -157,6 +157,8 @@ class_correct = list (0. for i in range(10))
 class_total = list(0. for i in range(10))
 
 
+class_correct = list(0. for i in range(10))
+class_total = list(0. for i in range(10))
 with torch.no_grad():
     for data in testloader:
         images, labels = data
@@ -168,5 +170,14 @@ with torch.no_grad():
             class_correct[label] += c[i].item()
             class_total[label] += 1
 
+
 for i in range(10):
-        print('Accuracy of %5s : %2d %%' % (classes[i], 100 * class_correct[i] / class_total[i]))
+    print('Accuracy of %5s : %2d %%' % (
+        classes[i], 100 * class_correct[i] / class_total[i]))
+
+## run on GPU
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(device)
+
+net.to(device)
+
